f8c00a8575f39fa3be24739e6e951ce4519afc8c
piriram <62399318+piriram@users.noreply.github.com>
2023-07-09 21:15:14 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 148.41 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,20 +1,23 @@
-# input : arr
-# 0~9의 값을 원소로 가지는 배열(중복 o)
-# 중복제거하여 출력
-# 중복시 앞에 것을 남김
-def solution(arr):
-    answer = []
-    # arr = sorted(arr)
-    tmp1=tmp2=0
 
-    for i in range(0,len(arr)):
-        if arr[i]==0 and tmp2==0:
-            tmp1=arr[i]
-            tmp2+=1
-            answer.append(arr[i])
-        elif arr[i]==tmp1:
-            tmp2+=1
-        else:
-            tmp1=arr[i]
-            answer.append(arr[i])
-    return answer
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+# 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+# 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+def solution(s):
+    # 함수를 완성하세요
+    a = []
+    for i in s:
+        if a[-1:] == [i]: continue
+        a.append(i)
+    return a
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print( solution( "133303" ))
