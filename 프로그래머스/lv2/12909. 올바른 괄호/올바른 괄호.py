0d017bfa3f68f218a514eeca1b5d502d00b6fe7f
piriram <62399318+piriram@users.noreply.github.com>
2023-07-09 20:49:34 +0900

    [level 2] Title: 올바른 괄호, Time: 8.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,8 @@
 def solution(s):
-    stack = []
-    for i in s:
-        if i == '(':  # '('는 stack에 추가
-            stack.append(i)
-        else:  # i == ')'인 경우
-            if stack != []:  # 괄호 짝이 ')'로 시작하면 False 반환
-                stack.pop()  # '('가 ')'와 짝을 이루면 stack에서 '(' 하나 제거
-            else:
-                return False
+    x = 0
+    for w in s:
+        if x < 0:
+            break
+        x = x+1 if w=="(" else x-1 if w==")" else x
+    return x==0
     
-    return stack==[]
