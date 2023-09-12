52e9af2ea071a15d1aaa85b6aa7d31717acdb828
piriram <62399318+piriram@users.noreply.github.com>
2023-09-12 12:33:01 +0900

    [level 1] Title: 폰켓몬, Time: 1.34 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,27 @@
+# N/2개 수집할 때 최대한 많은 종류의 폰켓몬 수집
+# 계수정렬?
+# best -> 하나씩만 가져가기
+# 1이상인 것이 n/2개가 되는지 확인
+# 1 이상인 것의 갯수 : valid_index
+# n/2 
+import math
+def solution(nums):
+    answer = 0
+    n = len(nums)
+    counting = dict()
+    for i in nums:
+        if i in counting:
+            counting[i] += 1
+        else:
+            counting[i] = 1
+    sorted_counting = sorted(counting.items(), key=lambda x: x[0])
+    
+    size = len(sorted_counting)
+    
+    if (n/2)<=size: 
+        # 가져가는 개수<=전체 종류 => 종류당 하나씩 총 n/2 종류 가져감
+        answer = n/2
+    else: 
+        # 가져가는 개수 > 전체 종류 => 중복되는 종류 있으므로 전체 종류는 종류 개수가 됨
+        answer = size
+    return answer
