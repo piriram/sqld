9ed70591ad8ccb73e45243afdc72acff8c982eb0
piriram <62399318+piriram@users.noreply.github.com>
2023-09-12 09:54:02 +0900

    [level 1] Title: K번째수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,10 +1,8 @@
-# i~j 크롭
-# k번째 있는 수 -> i+k
 def solution(array, commands):
     answer = []
-    for cmd in commands:
-        i,j,k = cmd
-        clc = sorted(array[i-1:j])
-        answer.append(clc[k-1])
-
+    for i,j,k in commands:
+        print(f"i:{i},j:{j},k:{k}")
+        new_arr = sorted(array[i-1:j])
+        print(new_arr)
+        answer.append(new_arr[k-1])
     return answer
