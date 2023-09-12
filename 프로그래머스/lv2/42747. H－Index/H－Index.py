574df69848dcfe92a6549ee39bc63d3cb0627520
piriram <62399318+piriram@users.noreply.github.com>
2023-09-12 09:37:09 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,13 +1,15 @@
 def solution(citations):
-    ct = citations
     answer = 0
-    
+    listAnswer = []
+    setCitations = sorted(list(set(citations)),reverse=True)
+    maxSet = max(setCitations)
     citations.sort(reverse=True)
-    num = 0
-    print(citations)
-    for ct in citations:
-        num=ct
-        if answer >= num:
-            break
-        answer += 1
+    dictCitations = {x:0 for x in range(maxSet,-1,-1)}
+    for i in citations:
+        for l in dictCitations.keys():
+            if l <= i:
+                dictCitations[l] += 1
+    for x, y in dictCitations.items():
+        if x <= y:
+            return x
     return answer
